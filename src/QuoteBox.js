fc427bdc957306eccb6d3ed34cf3b40f16b1016e
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-02-24 00:24:57 -0300

    Tweet quote on click

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react'
 
-const  QuoteBox= () => {
+const  QuoteBox= () => {  
   const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
   let [quotes, setQuotes] = useState(null)
   let [quoteText, setQuoteText] = useState(null);
   let [quoteAuthor, setQuoteAuthor] = useState(null);
+  const tweetURL = `https://twitter.com/intent/tweet?text=${quoteText}-${quoteAuthor}`;
 
   useEffect(() => {
     fetch(URL)
@@ -32,7 +33,7 @@ const  QuoteBox= () => {
       <h1 id="text">{quoteText}</h1>
       <h1 id="author">{quoteAuthor}</h1>
       <button id="new-quote" onClick={getRandomQuote}>Get New Quote</button>
-      <a id="tweet-quote" href="www.twitter.com">Tweet Quote</a>
+      <a id="tweet-quote" target="_blank" rel="noreferrer" href={tweetURL}>Tweet Quote</a>
     </div>
   );
 }
