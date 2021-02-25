006a28b51b313309deb16cf22beae1e1a22b8ffe
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-02-24 21:06:22 -0300

    Fix variables initiation

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 
 const  QuoteBox= () => {  
-  const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+  const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
   let [quotes, setQuotes] = useState(null)
-  let [quoteText, setQuoteText] = useState(null);
-  let [quoteAuthor, setQuoteAuthor] = useState(null);
-  const tweetURL = `https://twitter.com/intent/tweet?text=${quoteText}-${quoteAuthor}`;
+  let [quoteText, setQuoteText] = useState(null)
+  let [quoteAuthor, setQuoteAuthor] = useState(null)
+  let [fetchCounter, setFetchCounter] = useState(0)
+  const tweetURL = `https://twitter.com/intent/tweet?text=${quoteText}-${quoteAuthor}`
 
   useEffect(() => {
     fetch(URL)
@@ -13,26 +14,23 @@ const  QuoteBox= () => {
         return res.json()
       })
       .then(data => {
-        setQuotes(data.quotes)
+        let randomNumber = Math.floor(Math.random() * data.quotes.length)
+        setQuotes(quotes)
+        setQuoteText(data.quotes[randomNumber].author)
+        setQuoteAuthor(data.quotes[randomNumber].quote)
+        console.log(`The random generated number is ${randomNumber}`)
+        console.log(`The length of the quotes array is ${data.quotes.length}`)
+        console.log(`The current quote text is ${quoteText}`)
+        console.log(`The current quote author is ${quoteAuthor}`)
+        console.log(`The current fetch counter is ${fetchCounter}`)
       })
-  }, []);
-
-  function getRandomQuote() {
-    let randomNumber = Math.floor(Math.random() * 102)
-    console.log(randomNumber)
-    setQuoteText(quotes[randomNumber].author);
-    setQuoteAuthor(quotes[randomNumber].quote)
-  }
-
-  console.log(quotes)
-  console.log(quoteText)
-  console.log(quoteAuthor)
+  }, [fetchCounter]);
 
   return (
     <div id="quote-box">
       <h1 id="text">{quoteText}</h1>
       <h1 id="author">{quoteAuthor}</h1>
-      <button id="new-quote" onClick={getRandomQuote}>Get New Quote</button>
+      <button id="new-quote" onClick={() => setFetchCounter(fetchCounter + 1)}>Get New Quote</button>
       <a id="tweet-quote" target="_blank" rel="noreferrer" href={tweetURL}>Tweet Quote</a>
     </div>
   );
